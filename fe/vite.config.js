a279efb955f485fdfcd0fe88dc017f14dfa02521
howlil <muhammadulilabshar27@gmail.com>
2024-09-21 12:46:12 +0700

    update to vercel

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -7,6 +7,9 @@ const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
